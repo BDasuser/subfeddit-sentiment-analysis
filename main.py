c15dd879872bffd85caac7e00ce9e7bf3da4e68f
BDasuser <bhagabati.das1992@gmail.com>
2024-06-09 23:31:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ port = int(settings.get("app_port", 8000))
 
 app.include_router(router)
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__": 
     uvicorn.run("main:app", host=ip, port=port)
+
