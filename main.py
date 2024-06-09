36f989af97fd6e8233ad34a8006f7d2f36c08787
BDasuser <bhagabati.das1992@gmail.com>
2024-06-09 23:04:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+from app.config.configs import settings
+from app.controller.controller import router
+
+
+app = FastAPI(title="App")
+
+ip = settings.get("app_host", "localhost")
+port = int(settings.get("app_port", 8000))
+
+app.include_router(router)
+
+if __name__ == "__main__":
+    
+    uvicorn.run("main:app", host=ip, port=port)
